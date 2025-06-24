b10e2ace1153c72b96039c4eb71cb5e3532ce93f
Patrick Swickard <swickape@hotmail.com>
2025-06-24 11:04:59 -0400

    parselinks checkin

--- bytype/parselinks.py
+++ bytype/parselinks.py
@@ -1,18 +1,25 @@
 """Demonstrate regex replace by line"""
 import re
 
-file = 'website.html'
+#file = 'website.html'
+file = 'conference_presentations.html'
 with open(file,'r',encoding='utf-8') as myinfile:
   lines = myinfile.read().splitlines()
+
+
+url_set = set()
 
 # find and report
 for thisline in lines:
   branch = re.search(r"<span\s+class=\"field-content\">\s*<a\s+href=\"(.*?)\"",thisline)
   if branch:
-    print('https://targethiv.org' + branch.group(1))
-
+    thisurl = 'https://targethiv.org' + branch.group(1)
+    url_set.add(thisurl)
 
 ## find and report
 #for thisline in lines:
 #  newline = re.sub(r"\W","",thisline)
 #  print(newline)
+
+for thisurl in url_set:
+  print(thisurl)
